8a127621e1c53196b7c59b0444a40a834978a14f
jiachintang <85091432+jiachintang@users.noreply.github.com>
2021-06-30 10:59:11 +0800

    read less color

--- Assignment2/readmore.js
+++ Assignment2/readmore.js
@@ -7,10 +7,13 @@ function readmore() {
     if (dots.style.display === "none") {
       dots.style.display = "inline";
       readmorebtn.innerHTML =("Read More &nbsp;" + '<i class="fa fa-arrow-circle-o-down" aria-hidden="true"></i>');
+      readmorebtn.style. backgroundColor = '#2196f3';
       moreText.style.display = "none";
     } else {
       dots.style.display = "none";
+      
       readmorebtn.innerHTML = ("Read less &nbsp;"+ '<i class="fa fa-arrow-circle-o-up" aria-hidden="true"></i>');
+      readmorebtn.style. backgroundColor = 'Red';
       moreText.style.display = "inline";
     }
   }
