41cff2c7533e55c9519d3fd7d577ef3560b8d603
jiachintang <85091432+jiachintang@users.noreply.github.com>
2021-06-30 20:45:50 +0800

    .

--- Assignment2/mobilenav.js
+++ Assignment2/mobilenav.js
@@ -1,5 +1,5 @@
 function mobilenav() {
-    var x = document.getElementById("mobile");
+    var x = document.getElementById("mobileNav");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
