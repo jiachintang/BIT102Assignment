016d34be072ec712521b0df5c6e723b457267557
Joel Cheah <gjoelcheah@gmail.com>
2021-07-02 15:35:16 +0800

    updated header navi footer

--- Assignment2/Javascript/Header_Navi_Footer.js
+++ Assignment2/Javascript/Header_Navi_Footer.js
@@ -12,3 +12,21 @@ function clickHamburger() {
         miscNav.className = "miscNav";
     }
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    // Get the navbar
+    var navbar = document.getElementById("navbarJS");
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+    //Only have Sticky when reach the navbar position
+    if (window.pageYOffset >= sticky) {
+        navbar.className= "navbar sticky"
+        document.getElementById("demo").innerHTML ="fuck yes";
+    } else {
+        navbar.className= "navbar"
+        document.getElementById("demo").innerHTML = "fuck no";
+    }
+}
