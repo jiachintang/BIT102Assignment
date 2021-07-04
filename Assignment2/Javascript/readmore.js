102773efb912e9bcad9d3a0c89bd728b2369ea3e
jiachintang <85091432+jiachintang@users.noreply.github.com>
2021-07-04 08:38:02 +0800

    change color

--- Assignment2/Javascript/readmore.js
+++ Assignment2/Javascript/readmore.js
@@ -6,14 +6,14 @@ function readmore() {
     if (dots.style.display === "none") {
       dots.style.display = "inline";
       readmorebtn.innerHTML =("Read More &nbsp;" + '<i class="fa fa-arrow-circle-o-down" aria-hidden="true"></i>');
-      readmorebtn.style. backgroundColor = '#2196f3';
+      readmorebtn.style. backgroundColor = '#C77536';
       moreText.style.display = "none";
   
     } 
     else {
       dots.style.display = "none";
       readmorebtn.innerHTML = ("Read less &nbsp;"+ '<i class="fa fa-arrow-circle-o-up" aria-hidden="true"></i>');
-      readmorebtn.style. backgroundColor = 'Red';
+      readmorebtn.style. backgroundColor = '#ed7014';
       moreText.style.display = "inline";
       moreText.className="fadeIn";
     }
